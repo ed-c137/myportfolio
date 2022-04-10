a366fe19708aed119eb62540a0c85d9ee8a0f9ae
edrick <edrick613@gmail.com>
2022-04-10 17:44:32 +0100

    added form with capchca and resume

--- src/pages/Aboutme.vue
+++ src/pages/Aboutme.vue
@@ -24,27 +24,41 @@
      Check out the <a href="https://edrick.xyz/projects/">projects</a>  page to see a highlight of the open-source projects I've made, and <a href="https://edrick.xyz/">articles</a> to see everything I've written.
    </p>
   </section>
-  <section>
+  <section class="technology">
     <h4>Tech Stack Used</h4>
-    <div>
-      <h5>Currently Using</h5>
-      <ul>
-        <li>Vuejs</li>
-        <li>Gridsome</li>
-        <li>Bootstrap</li>
-        <li>SCSS</li>
-        <li>Wordpress</li>
-        <li>Gulp 4</li>
-        <li>Figma</li>
-      </ul>
-      <h5>Previosly used</h5>
-      <ul>
-        <li>Laravel</li>
-        <li>React</li>
-        <li>Gatsby</li>
-      </ul>
+    <div class="tech-col">
+      <div>
+        <h5>Currently Using</h5>
+        <ul>
+          <li>Vuejs</li>
+          <li>Gridsome</li>
+          <li>Bootstrap</li>
+          <li>SCSS</li>
+          <li>Wordpress</li>
+          <li>Gulp 4</li>
+          <li>Figma</li>
+        </ul>
+      </div>
+      <div>
+        <h5>Previosly used</h5>
+        <ul>
+          <li>Laravel</li>
+          <li>React</li>
+          <li>Gatsby</li>
+        </ul>
+      </div>
     </div>
   </section>
+    <section>
+    <div class="dwn-cv">
+        <a aria-label="Edrick on LinkedIn" href="/EdCV.pdf" class="downloadcvbtn" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 bg-green-300 p-2 rounded-full" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M16 2l5 5v14.008a.993.993 0 0 1-.993.992H3.993A1 1 0 0 1 3 21.008V2.992C3 2.444 3.445 2 3.993 2H16zm-4 9.5a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5zM7.527 17h8.946a4.5 4.5 0 0 0-8.946 0z" fill="#626262"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg>
+      <span>
+        Download CV
+      </span>   
+      </a> 
+   </div>
+    </section>
   </Layout>
  
 </template>
@@ -77,4 +91,30 @@ export default {
       }
     }
   }
+.technology{
+  h4{
+    text-align: center;
+  }
+}
+.tech-col{
+    display: flex;
+    justify-content: space-around;
+  }
+.dwn-cv{
+  display: flex;
+  justify-content: center;
+  margin: 3rem;
+  .downloadcvbtn{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: orange;
+    color: #333;
+    width: 10rem;
+    padding: .3rem;
+    text-decoration: none;
+    border-radius: 4px;
+  }
+
+}
 </style>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -12,10 +12,7 @@
           <form
             name="contact"
             method="post"
-            v-on:submit.prevent="handleSubmit"
-            action="/success/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
+            v-on:submit.prevent="handleSubmitForm"
           >
             <input type="hidden" name="form-name" value="contact" />
             <p hidden>
@@ -24,21 +21,38 @@
               </label>
             </p>
             <div class="sender-info">
-              <div class="form-group">
+              <div class="form-group" >
                 <label for="name" class="label" >Your name</label>
-                <input type="text" name="name" v-model="formData.name" />
+                <input type="text" name="name" v-model="form.name" />
+                 <!-- <div class="input-errors" v-for="(error, index) of v$.form.name.$errors" :key="index">
+                  <div class="error-msg">{{ error.$message }}</div>
+                </div> -->
               </div>
-              <div class="form-group">
+              <div class="form-group" >
                 <label for="email">Your email</label>
-                <input type="email" name="email" v-model="formData.email" />
+                <input type="email" name="email" v-model="form.email" />
+                 <!-- <div class="input-errors" v-for="(error, index) of v$.form.email.$errors" :key="index">
+                  <div class="error-msg">{{ error.$message }}</div>
+                </div> -->
               </div>
             </div>
 
-            <div class="form-group message-wrapper">
+            <div class="form-group message-wrapper" >
               <label for="message">Message</label>
-              <textarea name="message" v-model="formData.message"></textarea>
+              <textarea name="message" v-model="form.message"></textarea>
+              <!-- <div class="input-errors" v-for="(error, index) of v$.form.message.$errors" :key="index">
+                <div class="error-msg">{{ error.$message }}</div>
+              </div> -->
             </div>
-
+            <!-- <div class="buttons-w">
+              <button class="btn btn-primary">Login</button>
+            </div> -->
+            <div class="placehoder-wrap">
+              <!-- <img src="https://via.placeholder.com/100x40/000000/FFFFFF/?text=09369"  alt=""> -->
+              <img :src="cstring" height="40" width="100" alt="">
+              <input type="text" placeholder=" Type Caphca" v-model="captured">
+            </div>
+            <div class="message" :class="{ error: isError, success: isSuccess }">{{ message }}</div>
             <button type="submit">Send</button>
           </form>
         </div>
@@ -50,10 +64,30 @@
 </template>
 
 <script>
+import SheetDB from 'sheetdb-js';
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      formData: {},
+      cData: [
+        { 'name' : '01386' },
+        { 'name' : '37287' },
+        { 'name' : '27390' },
+        { 'name' : '62939' },
+        { 'name' : '32692' },
+      ],
+      cNum: null,
+      cstring: null,
+      captured: null,
+      isError: false,
+      isSuccess: false,
+      message: '', 
+      form: {
+        name: '',
+        email: '',
+        message: '',
+      },
     }
   },
   methods: {
@@ -73,8 +107,66 @@ export default {
       })
       .then(() => this.$router.push('/success'))
       .catch(error => alert(error))
+    },
+
+    selectCapchca(){
+      let x = Math.floor((Math.random() * 4) + 1);
+      let y = x - 1;
+      this.cNum = y;
+      let iname = this.cData[y].name;
+      this.cstring = `https://via.placeholder.com/200x80.png/000000/FFFFFF/?text=${iname}`
+    },
+   
+    validtxt(name) {
+      let validNamePattern = new RegExp("^[a-zA-Z]+(?:[-'\\s][a-zA-Z]+)*$");
+      if (validNamePattern.test(name)){
+        return true;
+      }
+      return false;
+    },
+    handleSubmitForm(){
+     let name = this.form.name;
+     let email = this.form.email;
+     let message = this.form.message;
+      let that = this;
+    //   SheetDB.create('https://sheetdb.io/api/v1/jvxqz4upthz7a', { sheet: 'Sheet1', data:{
+    //     name: name, email : email, message: message
+    //   }}).then(function(result){
+    //     console.log(result);
+    //     that.$router.push('/success');
+    // }, function(error){
+    //   console.log(error);
+    // });
+    if(this.cData[this.cNum].name == this.captured ){
+        axios.post('https://sheetdb.io/api/v1/jvxqz4upthz7a',{ sheet: 'Sheet1', data:{
+          name: name, email : email, message: message
+        }}).then( response => {
+          console.log(response.data);
+          if(response.data.created >= 1){
+            this.isSuccess = true;
+            this.message = `Thanks ${this.form.name}! I will get back to you`
+          }
+      });
+    } else {
+      this.isError = true;
+      this.message = "The capthca didnt match the image"
+      console.log('capchca didnt match');
     }
+
+    }
+  
+
   },
+  // created(){
+  //   SheetDB.read('https://sheetdb.io/api/v1/jvxqz4upthz7a', {}).then(function(result){
+  //     console.log(result);
+  //   }, function(error){
+  //     console.log(error);
+  //   });
+  // }
+  created(){
+    this.selectCapchca();
+  }
 
 }
 </script>
@@ -161,4 +253,34 @@ export default {
       flex-direction: column;
     }
   }
+
+  .placehoder-wrap{
+    display: flex;
+    padding: 1.8rem 0;
+    img{
+
+    }
+    input{
+      width: 7rem;
+      margin-left: 1rem;
+      font-size: 0.9rem;
+      padding: 0 0.6rem;
+    }
+  }
+
+  .message{
+
+    }
+    .error{
+      color: #842029;
+      background-color: #f8d7da;
+      border-color: #f5c2c7;
+      padding: 0.5rem;
+    }
+    .success{
+      color: #0f5132;
+      background-color: #d1e7dd;
+      border-color: #badbcc;
+      padding: 0.5rem;
+    }
 </style>
